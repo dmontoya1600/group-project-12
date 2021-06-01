42963e5715082e7191cf1e4439ec4886a8376402
Joe <Joe.zhang229@gmail.com>
2021-06-01 13:13:02 -0700

    movie association to user

--- db/models/movie.js
+++ db/models/movie.js
@@ -1,33 +1,38 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Movie = sequelize.define('Movie', {
-    title: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    genre: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    actors: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    director: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    avgRating: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: Sequelize.TEXT,
-      allowNull: false,
-    }
-  }, {});
-  Movie.associate = function(models) {
-    // associations can be defined here
-  };
-  return Movie;
+	const Movie = sequelize.define(
+		'Movie',
+		{
+			title: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			genre: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			actors: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			director: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			avgRating: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			description: {
+				type: Sequelize.TEXT,
+				allowNull: false,
+			},
+		},
+		{}
+	);
+	Movie.associate = function (models) {
+		// associations can be defined here
+		Movie.belongsTo(models.User, { foreignKey: 'moviesId' });
+	};
+	return Movie;
 };
