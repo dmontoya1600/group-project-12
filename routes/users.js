52fdb0bd98775a21c2e152cdd57bb022e48f8829
Damian Montoya <dmontoya1600@gmail.com>
2021-06-03 18:48:23 -0500

    completed user styling for now

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const { csrfProtection, asyncHandler } = require('./utils');
 const restoreUser = require('../auth');
 const { User, Movie } = require('../db/models');
+const images = require('./images');
+
 
 /* GET users listing. */
 router.get('/', asyncHandler(async(req, res, next) => {
 	const allMovies = await Movie.findAll();
-
+	allMovies.forEach( movie => movie.image = images[movie.id])
 	const horror = allMovies.filter(movie => movie.genre.includes('Horror'))
 	const action = allMovies.filter(movie => movie.genre.includes('Action'))
 	const adventure = allMovies.filter(movie => movie.genre.includes('Adventure'))
@@ -17,7 +19,6 @@ router.get('/', asyncHandler(async(req, res, next) => {
 	const fantasy = allMovies.filter(movie => movie.genre.includes('Fantasy'))
 	const sciFi = allMovies.filter(movie => movie.genre.includes('Science Fiction'))
 
-  
 
 	res.render('users', {
 		horror,
@@ -27,7 +28,8 @@ router.get('/', asyncHandler(async(req, res, next) => {
 		drama,
 		thriller,
 		fantasy,
-		sciFi
+		sciFi,
+		images
 	});
 }));
 
