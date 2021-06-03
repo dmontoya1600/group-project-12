5608487b1ccd78480c9a4a9d95a7c0259760e17c
Joe <Joe.zhang229@gmail.com>
2021-06-03 10:25:45 -0700

    genre migration character limit

--- db/migrations/20210601181212-create-movie.js
+++ db/migrations/20210601181212-create-movie.js
@@ -1,47 +1,47 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Movies', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      title: {
-        type: Sequelize.STRING(50),
-        allowNull: false
-      },
-      genre: {
-        type: Sequelize.STRING(20),
-        allowNull: false,
-      },
-      actors: {
-        type: Sequelize.STRING(100),
-        allowNull: false
-      },
-      director: {
-        type: Sequelize.STRING(100),
-        allowNull: false
-      },
-      avgRating: {
-        type: Sequelize.DECIMAL(2,1)
-      },
-      description: {
-        type: Sequelize.TEXT,
-        allowNull: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Movies');
-  }
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('Movies', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			title: {
+				type: Sequelize.STRING(50),
+				allowNull: false,
+			},
+			genre: {
+				type: Sequelize.STRING(50),
+				allowNull: false,
+			},
+			actors: {
+				type: Sequelize.STRING(100),
+				allowNull: false,
+			},
+			director: {
+				type: Sequelize.STRING(100),
+				allowNull: false,
+			},
+			avgRating: {
+				type: Sequelize.DECIMAL(2, 1),
+			},
+			description: {
+				type: Sequelize.TEXT,
+				allowNull: false,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('Movies');
+	},
 };
