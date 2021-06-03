81b4de7cd8fd4b0e17911c17c527c0975ecbc4d0
Damian Montoya <dmontoya1600@gmail.com>
2021-06-03 13:01:27 -0500

    setting up heroku

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@ const {
   db: { username, password, database, host },
 } = require('./index');
 
+// seederStorage: 'sequelize',
 module.exports = {
   development: {
     username,
@@ -9,6 +10,10 @@ module.exports = {
     database,
     host,
     dialect: 'postgres',
-    seederStorage: 'sequelize',
   },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  }
 };
