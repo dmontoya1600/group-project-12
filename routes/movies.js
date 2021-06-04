e21e15fc00ffb846014e11214359bc0b19a8f59c
Damian Montoya <dmontoya1600@gmail.com>
2021-06-04 17:35:30 -0500

    DONE WITH AJAX. FINALLY

--- routes/movies.js
+++ routes/movies.js
@@ -31,6 +31,10 @@ router.get(
 	asyncHandler(async (req, res) => {
 		const movieId = req.params.id;
 		const movie = await db.Movie.findByPk(movieId);
+		const favorite = await db.Favorite.findOne({
+			movieId: movieId
+		})
+		console.log('THIS IS THE FAVORITE ID', favorite)
 		const allReviews = await db.Review.findAll({
 			where: { movieId: movie.id },
 			include: ['Movie'],
@@ -40,7 +44,7 @@ router.get(
 		if (!movie) {
 			res.status(404);
 			res.send('movie cannot be found!');
-		} else if (req.session.auth) {
+		} else if (req.session.auth.userId) {
 			const userId = req.session.auth.userId;
 			const userReviews = await db.Review.findAll({
 				where: { movieId: movie.id, userId: userId },
@@ -50,6 +54,8 @@ router.get(
 				title: `${movie.title}`,
 				movie,
 				userReviews,
+				allReviews,
+				favorite
 			});
 		} else {
 			res.render('movie', {
